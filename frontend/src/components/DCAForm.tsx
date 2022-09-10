3b774e1c0508495a25d805cfe269d7252cb43cb4
Piotr Durniat <piotr@durniat.com>
2022-09-10 18:36:47 +0200

    Form paper

--- frontend/src/components/DCAForm.tsx
+++ frontend/src/components/DCAForm.tsx
@@ -6,6 +6,8 @@ import {
   InputAdornment,
   InputLabel,
   OutlinedInput,
+  Paper,
+  Typography,
 } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
@@ -26,46 +28,53 @@ const DCAForm: FC<PropTypes> = ({
 }) => {
   return (
     <Box sx={{ marginBottom: 2 }}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          refetch();
-        }}
-      >
-        <FormControl sx={{ m: 1 }} variant="outlined">
-          <InputLabel htmlFor="ticker-input">Ticker symbol</InputLabel>
-          <OutlinedInput
-            id="ticker-input"
-            type="text"
-            value={ticker}
-            onChange={(e) => setTicker(e.target.value)}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => refetch()}
-                  edge="end"
-                >
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Ticker symbol"
-          />
-        </FormControl>
+      <Paper sx={{ padding: 3 }}>
+        <Typography variant="h4" mb={2}>
+          Dollar Cost Averaging Calculator
+        </Typography>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            refetch();
+          }}
+        >
+          <FormControl variant="outlined" sx={{ marginRight: 2 }}>
+            <InputLabel htmlFor="ticker-input">Ticker symbol</InputLabel>
+            <OutlinedInput
+              id="ticker-input"
+              type="text"
+              value={ticker}
+              onChange={(e) => setTicker(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => refetch()}
+                    edge="end"
+                  >
+                    <Search />
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Ticker symbol"
+            />
+          </FormControl>
 
-        <FormControl sx={{ m: 1 }} variant="outlined">
-          <InputLabel htmlFor="amount-input">Amount</InputLabel>
-          <OutlinedInput
-            id="amount-input"
-            type="number"
-            label="amount"
-            value={amount}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            onChange={(e) => setAmount(Number(e.target.value))}
-          ></OutlinedInput>
-        </FormControl>
-      </form>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="amount-input">Amount</InputLabel>
+            <OutlinedInput
+              id="amount-input"
+              type="number"
+              label="amount"
+              value={amount}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              onChange={(e) => setAmount(Number(e.target.value))}
+            ></OutlinedInput>
+          </FormControl>
+        </form>
+      </Paper>
     </Box>
   );
 };
